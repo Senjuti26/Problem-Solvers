b18dd8dde3a37d52d911e85a722d05d6df1a2107
Senjuti26 <amisenjuti.26@gmail.com>
2018-05-05 00:30:45 +0530

    Add files via upload

---
+++ src/login_check.php
@@ -0,0 +1,31 @@
+<?php
+	include("./includes/connection.php");
+	
+	if(isset($_POST["email"]) && isset($_POST["password"])){
+		$email = $_POST["email"];
+		$password = $_POST["password"];
+		
+		$sql = "SELECT * FROM user WHERE email='".$email."' and password='".md5($password)."'";
+		$q = mysqli_query($con,$sql);
+		if(mysqli_num_rows($q)==1){
+			session_start();
+			while($rs = mysqli_fetch_array($q)){
+				$_SESSION["user_id"] = $rs["user_id"];
+				$_SESSION["admin"] = $rs["admin"];
+			}
+			header("location:./index.php");
+		}
+		else{
+			?>
+			<script>
+				alert("Invalid email or password.!!");
+				window.location="./login.php";
+			</script>
+			<?php
+		}
+	}
+	else{
+		header("location:./errors/illigle_operation.php");
+	}
+	mysqli_close($con);
+?>
